d3b9c2757500de259e85b32d7cbce90eadb9d1e2
Eonji-sw <ejkim0625@gmail.com>
2024-03-03 22:43:02 +0900

    [ADD] : setting android project

---
+++ android/app/src/main/java/com/kotlin/kiumee/core/base/BindingActivity.kt
@@ -0,0 +1,35 @@
+package com.kotlin.kiumee.core.base
+
+import android.os.Build
+import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
+import androidx.annotation.LayoutRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import com.kotlin.kiumee.core.util.context.hideKeyboard
+
+abstract class BindingActivity<T : ViewDataBinding>(
+    @LayoutRes private val layoutRes: Int
+) : AppCompatActivity() {
+    protected lateinit var binding: T
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, layoutRes)
+        binding.lifecycleOwner = this
+        if (Build.VERSION.SDK_INT >= 30) { // API 30 에 적용
+            WindowCompat.setDecorFitsSystemWindows(window, false)
+        }
+        initView()
+    }
+
+    protected abstract fun initView()
+
+    override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
+        hideKeyboard(currentFocus ?: View(this))
+        return super.dispatchTouchEvent(ev)
+    }
+}
--- android/app/src/main/java/com/kotlin/kiumee/presentation/MainActivity.kt
+++ android/app/src/main/java/com/kotlin/kiumee/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.kotlin.kiumee
+package com.kotlin.kiumee.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.kotlin.kiumee.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
