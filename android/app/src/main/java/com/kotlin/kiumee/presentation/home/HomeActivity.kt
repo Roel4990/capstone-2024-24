e6a5945e84b4877376987efcd53be961cf0906ae
Eonji-sw <ejkim0625@gmail.com>
2024-03-07 01:51:25 +0900

    [FEAT] : home activity 구현

---
+++ android/app/src/main/java/com/kotlin/kiumee/presentation/home/HomeActivity.kt
@@ -0,0 +1,20 @@
+package com.kotlin.kiumee.presentation.home
+
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+import com.kotlin.kiumee.R
+import com.kotlin.kiumee.core.base.BindingActivity
+import com.kotlin.kiumee.databinding.ActivityHomeBinding
+
+class HomeActivity : BindingActivity<ActivityHomeBinding>(R.layout.activity_home) {
+    override fun initView() {
+        initBackground()
+    }
+
+    private fun initBackground() {
+        Glide.with(this)
+            .load("https://cdn.newscj.com/news/photo/202307/3043256_3048062_3725.jpg")
+            .centerCrop()
+            .into(binding.ivHomeBackground)
+    }
+}
