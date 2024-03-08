3e940d056c10d200d88ea31329cf050802c68d9b
Eonji-sw <ejkim0625@gmail.com>
2024-03-08 10:42:14 +0900

    [ADD] : menu select fragment 추가

---
+++ android/app/src/main/java/com/kotlin/kiumee/presentation/menuselect/MenuSelectFragment.kt
@@ -0,0 +1,11 @@
+package com.kotlin.kiumee.presentation.menuselect
+
+import com.kotlin.kiumee.R
+import com.kotlin.kiumee.core.base.BindingFragment
+import com.kotlin.kiumee.databinding.FragmentMenuSelectBinding
+
+class MenuSelectFragment :
+    BindingFragment<FragmentMenuSelectBinding>(R.layout.fragment_menu_select) {
+    override fun initView() {
+    }
+}
