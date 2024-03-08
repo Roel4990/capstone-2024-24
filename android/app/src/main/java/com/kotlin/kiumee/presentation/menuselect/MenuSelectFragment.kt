ee41b2895731c94331749b8cbcebc4ce1914ac22
Eonji-sw <ejkim0625@gmail.com>
2024-03-08 11:19:37 +0900

    [UI] : menu select fragment test replace 구현

--- android/app/src/main/java/com/kotlin/kiumee/presentation/menuselect/MenuSelectFragment.kt
+++ android/app/src/main/java/com/kotlin/kiumee/presentation/menuselect/MenuSelectFragment.kt
@@ -3,9 +3,17 @@ package com.kotlin.kiumee.presentation.menuselect
 import com.kotlin.kiumee.R
 import com.kotlin.kiumee.core.base.BindingFragment
 import com.kotlin.kiumee.databinding.FragmentMenuSelectBinding
+import com.kotlin.kiumee.presentation.menucartselect.MenuCartSelectFragment
 
 class MenuSelectFragment :
     BindingFragment<FragmentMenuSelectBinding>(R.layout.fragment_menu_select) {
     override fun initView() {
+        binding.btnMenuSelectTest.setOnClickListener {
+            requireActivity().supportFragmentManager.beginTransaction().apply {
+                replace(R.id.fcv_chat, MenuCartSelectFragment())
+                addToBackStack(null) // 뒤로 가기 동작을 위해 백스택에 추가
+                commit()
+            }
+        }
     }
 }
