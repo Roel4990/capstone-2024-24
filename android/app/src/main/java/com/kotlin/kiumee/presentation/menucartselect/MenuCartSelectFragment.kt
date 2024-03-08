79c73aa17bccc5de1c45ff4a0f7ec883c0939e75
Eonji-sw <ejkim0625@gmail.com>
2024-03-08 11:09:19 +0900

    [ADD] : menu cart select fragment 생성

---
+++ android/app/src/main/java/com/kotlin/kiumee/presentation/menucartselect/MenuCartSelectFragment.kt
@@ -0,0 +1,11 @@
+package com.kotlin.kiumee.presentation.menucartselect
+
+import com.kotlin.kiumee.R
+import com.kotlin.kiumee.core.base.BindingFragment
+import com.kotlin.kiumee.databinding.FragmentMenuCartSelectBinding
+
+class MenuCartSelectFragment :
+    BindingFragment<FragmentMenuCartSelectBinding>(R.layout.fragment_menu_cart_select) {
+    override fun initView() {
+    }
+}
