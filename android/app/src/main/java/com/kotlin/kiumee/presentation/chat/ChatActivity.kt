e3fc419e0459c4030a3cc742da46fa5b7061c693
Eonji-sw <ejkim0625@gmail.com>
2024-03-14 15:51:05 +0900

    [FEAT] : chat item decoration 적용

--- android/app/src/main/java/com/kotlin/kiumee/presentation/chat/ChatActivity.kt
+++ android/app/src/main/java/com/kotlin/kiumee/presentation/chat/ChatActivity.kt
@@ -18,11 +18,14 @@ class ChatActivity : BindingActivity<ActivityChatBinding>(R.layout.activity_chat
                     Chat(1, "키우미에게 대화를 시작해주세요!"),
                     Chat(0, "안녕."),
                     Chat(1, "안녕하세요!"),
+                    Chat(0, "나는 배가 고파."),
                     Chat(0, "여기에서 가장 잘 나가는 메뉴가 뭐야?"),
-                    Chat(1, "저희 매장에서 제일 잘 나가는 메뉴는 아이스 아메리카노입니다."),
+                    Chat(1, "저희 매장에서 제일 잘 나가는 메뉴는 아이스 아메리카노입니다.")
                 )
             )
         }
+
+        binding.rvChat.addItemDecoration(ChatItemDecoration(this))
     }
 
     private fun initActivityFragment() {
