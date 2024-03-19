6d5a7c6e98e835f329077c09b87bb648b0284059
Eonji-sw <ejkim0625@gmail.com>
2024-03-20 01:24:03 +0900

    [FEAT] : menu select fragment initTabPager 메서드 추가

--- android/app/src/main/java/com/kotlin/kiumee/presentation/menuselect/MenuSelectFragment.kt
+++ android/app/src/main/java/com/kotlin/kiumee/presentation/menuselect/MenuSelectFragment.kt
@@ -1,16 +1,42 @@
 package com.kotlin.kiumee.presentation.menuselect
 
+import com.google.android.material.tabs.TabLayoutMediator
 import com.kotlin.kiumee.R
 import com.kotlin.kiumee.core.base.BindingFragment
 import com.kotlin.kiumee.databinding.FragmentMenuSelectBinding
-import com.kotlin.kiumee.presentation.menucartselect.MenuCartSelectFragment
+import com.kotlin.kiumee.presentation.orderfinish.OrderFinishFragment
 
 class MenuSelectFragment :
     BindingFragment<FragmentMenuSelectBinding>(R.layout.fragment_menu_select) {
     override fun initView() {
-        binding.btnMenuSelectTest.setOnClickListener {
+        //initTabPager()
+        initPayBtnClickListener()
+    }
+
+//    private fun initTabPager() {
+//        val tabLayout = binding.layoutFragmentMenuSelectTab
+//        val viewPager = binding.vpMenuSelect
+//
+//        val menuTabAdapter = MenuSelectAdapter(this)
+//        viewPager.adapter = menuTabAdapter
+//
+//        val tabs = listOf(
+//            "커피(HOT)",
+//            "커피(ICE)",
+//            "에이드&티",
+//            "음료",
+//            "디저트"
+//        )
+//
+//        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+//            tab.text = tabs[position]
+//        }.attach()
+//    }
+
+    private fun initPayBtnClickListener() {
+        binding.btnMenuSelectCartPay.setOnClickListener {
             requireActivity().supportFragmentManager.beginTransaction().apply {
-                replace(R.id.fcv_chat, MenuCartSelectFragment())
+                replace(R.id.fcv_chat, OrderFinishFragment())
                 addToBackStack(null) // 뒤로 가기 동작을 위해 백스택에 추가
                 commit()
             }
