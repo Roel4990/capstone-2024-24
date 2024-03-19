d27df3fb7ecdb70ac80c01a1ccac18dcda03f949
Eonji-sw <ejkim0625@gmail.com>
2024-03-20 01:22:39 +0900

    [UI] : menu cart select fragment ui 구현

--- android/app/src/main/java/com/kotlin/kiumee/presentation/menucartselect/MenuCartSelectFragment.kt
+++ android/app/src/main/java/com/kotlin/kiumee/presentation/menucartselect/MenuCartSelectFragment.kt
@@ -3,14 +3,29 @@ package com.kotlin.kiumee.presentation.menucartselect
 import com.kotlin.kiumee.R
 import com.kotlin.kiumee.core.base.BindingFragment
 import com.kotlin.kiumee.databinding.FragmentMenuCartSelectBinding
-import com.kotlin.kiumee.presentation.orderfinish.OrderFinishFragment
+import com.kotlin.kiumee.presentation.menuselect.MenuSelectFragment
 
 class MenuCartSelectFragment :
     BindingFragment<FragmentMenuCartSelectBinding>(R.layout.fragment_menu_cart_select) {
     override fun initView() {
+        initNoBtnClickListener()
+        initYesBtnClickListener()
+    }
+
+    private fun initYesBtnClickListener() {
+        binding.btnMenuCartSelectYes.setOnClickListener {
+            requireActivity().supportFragmentManager.beginTransaction().apply {
+                replace(R.id.fcv_chat, MenuSelectFragment())
+                addToBackStack(null) // 뒤로 가기 동작을 위해 백스택에 추가
+                commit()
+            }
+        }
+    }
+
+    private fun initNoBtnClickListener() {
         binding.btnMenuCartSelectNo.setOnClickListener {
             requireActivity().supportFragmentManager.beginTransaction().apply {
-                replace(R.id.fcv_chat, OrderFinishFragment())
+                replace(R.id.fcv_chat, MenuSelectFragment())
                 addToBackStack(null) // 뒤로 가기 동작을 위해 백스택에 추가
                 commit()
             }
