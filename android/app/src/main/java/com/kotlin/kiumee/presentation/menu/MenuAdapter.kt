b24d917255568d555c49e3795d7199c27166c5d2
Eonji-sw <ejkim0625@gmail.com>
2024-03-21 23:07:43 +0900

    [MOD] : menu adapter 구현

--- android/app/src/main/java/com/kotlin/kiumee/presentation/menu/MenuAdapter.kt
+++ android/app/src/main/java/com/kotlin/kiumee/presentation/menu/MenuAdapter.kt
@@ -2,22 +2,38 @@ package com.kotlin.kiumee.presentation.menu
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.ListAdapter
+import com.kotlin.kiumee.core.view.ItemDiffCallback
 import com.kotlin.kiumee.databinding.ItemMenuBinding
 
-class MenuAdapter(private val menuList: MutableList<Menu>) :
-    RecyclerView.Adapter<MenuViewHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MenuViewHolder {
+class MenuAdapter(
+    private val click: (Menu, Int) -> Unit = { _, _ -> }
+) :
+    ListAdapter<Menu, MenuViewHolder>(
+        MenuAdapterDiffCallback
+    ) {
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): MenuViewHolder {
         val binding =
             ItemMenuBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return MenuViewHolder(binding)
+        return MenuViewHolder(binding, click)
     }
 
-    override fun onBindViewHolder(holder: MenuViewHolder, position: Int) {
-        holder.bind(menuList[position])
+    override fun onBindViewHolder(
+        holder: MenuViewHolder,
+        position: Int
+    ) {
+        holder.bind(currentList[position])
     }
 
-    override fun getItemCount(): Int {
-        return menuList.size
+    companion object {
+        private val MenuAdapterDiffCallback =
+            ItemDiffCallback<Menu>(
+                // 수정해야 함
+                onItemsTheSame = { old, new -> old == new },
+                onContentsTheSame = { old, new -> old == new }
+            )
     }
 }
