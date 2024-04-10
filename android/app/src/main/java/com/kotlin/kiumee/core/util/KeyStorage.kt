f86115d46cd4499577a9fae0005446338383ca46
Eonji-sw <ejkim0625@gmail.com>
2024-04-11 03:10:05 +0900

    [FEAT] : key storage 분리

---
+++ android/app/src/main/java/com/kotlin/kiumee/core/util/KeyStorage.kt
@@ -0,0 +1,9 @@
+package com.kotlin.kiumee.core.util
+
+object KeyStorage {
+    const val ACCESS_TOKEN = "access_token"
+    const val V1 = "v1"
+    const val LOGIN = "login"
+    const val BUSINESS = "business"
+    const val ITEMS = "items"
+}
