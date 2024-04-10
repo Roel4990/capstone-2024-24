1eb726be1867e1590ca87d32354d7f46e98be0ff
Eonji-sw <ejkim0625@gmail.com>
2024-04-11 03:11:56 +0900

    [FEAT] : store viewModel 구현(business)

---
+++ android/app/src/main/java/com/kotlin/kiumee/presentation/store/StoreViewModel.kt
@@ -0,0 +1,27 @@
+package com.kotlin.kiumee.presentation.store
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.kotlin.kiumee.core.view.UiState
+import com.kotlin.kiumee.data.dto.ServicePool
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+
+class StoreViewModel : ViewModel() {
+    private val _getStore = MutableStateFlow<UiState<List<StoreEntity>>>(UiState.Loading)
+    val getStore: StateFlow<UiState<List<StoreEntity>>> = _getStore
+
+    init {
+        getStore()
+    }
+
+    private fun getStore() = viewModelScope.launch {
+        runCatching {
+            ServicePool.businessApiService.getBusiness().data.map { it.toStoreEntity() }
+        }.fold(
+            { _getStore.value = UiState.Success(it) },
+            { _getStore.value = UiState.Failure(it.message.toString()) }
+        )
+    }
+}
