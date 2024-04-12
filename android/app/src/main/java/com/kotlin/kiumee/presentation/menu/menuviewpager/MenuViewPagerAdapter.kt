3da96bfb299bda56ebf48248fb4e6fa100369787
Eonji-sw <ejkim0625@gmail.com>
2024-04-13 01:21:44 +0900

    [MOD] : menu viewPager adapter 인자 type 수정

--- android/app/src/main/java/com/kotlin/kiumee/presentation/menu/menuviewpager/MenuViewPagerAdapter.kt
+++ android/app/src/main/java/com/kotlin/kiumee/presentation/menu/menuviewpager/MenuViewPagerAdapter.kt
@@ -4,8 +4,9 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Lifecycle
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.kotlin.kiumee.presentation.menu.CategoryEntity
 
-class MenuViewPagerAdapter(fm: FragmentManager, lifecycle: Lifecycle, private val tabList: List<String>) :
+class MenuViewPagerAdapter(fm: FragmentManager, lifecycle: Lifecycle, private val tabList: List<CategoryEntity>) :
     FragmentStateAdapter(fm, lifecycle) {
 
     override fun getItemCount(): Int = tabList.size
