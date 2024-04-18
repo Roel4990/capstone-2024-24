2be84f4ced5e53b276f52e147a23d6e3291086b4
Eonji-sw <ejkim0625@gmail.com>
2024-04-18 20:54:35 +0900

    [FEAT] : menuBtn adapter 구현

---
+++ android/app/src/main/java/com/kotlin/kiumee/presentation/menu/chat/menubtn/MenuBtnAdapter.kt
@@ -0,0 +1,35 @@
+package com.kotlin.kiumee.presentation.menu.chat.menubtn
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import com.kotlin.kiumee.core.view.ItemDiffCallback
+import com.kotlin.kiumee.databinding.ItemChatBtnBinding
+import com.kotlin.kiumee.presentation.menu.chat.Chat
+
+class MenuBtnAdapter() : ListAdapter<String, MenuBtnViewHolder>(MenuBtnAdapterDiffCallback) {
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): MenuBtnViewHolder {
+        val binding =
+            ItemChatBtnBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return MenuBtnViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(
+        holder: MenuBtnViewHolder,
+        position: Int
+    ) {
+        holder.bind(currentList[position])
+    }
+
+    companion object {
+        private val MenuBtnAdapterDiffCallback =
+            ItemDiffCallback<String>(
+                // 추후 수정해야 함
+                onItemsTheSame = { old, new -> old == new },
+                onContentsTheSame = { old, new -> old == new }
+            )
+    }
+}
