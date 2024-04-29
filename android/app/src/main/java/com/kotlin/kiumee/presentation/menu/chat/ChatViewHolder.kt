79b928a9f0e5239a43b982108c3539236ded0fc4
Eonji-sw <ejkim0625@gmail.com>
2024-04-29 19:18:31 +0900

    [MOD] : chatViewHolder -> chatJumiViewHolder / chatUserViewHolder 분리

--- android/app/src/main/java/com/kotlin/kiumee/presentation/menu/chat/ChatViewHolder.kt
+++ android/app/src/main/java/com/kotlin/kiumee/presentation/menu/chat/ChatViewHolder.kt
@@ -1,49 +1,44 @@
 package com.kotlin.kiumee.presentation.menu.chat
 
-import android.view.Gravity
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.kotlin.kiumee.R
-import com.kotlin.kiumee.core.util.context.colorOf
-import com.kotlin.kiumee.databinding.ItemChatBinding
+import com.kotlin.kiumee.databinding.ItemChatJumiBinding
+import com.kotlin.kiumee.databinding.ItemChatUserBinding
 import com.kotlin.kiumee.presentation.menu.chat.menubtn.MenuBtnAdapter
 
-class ChatViewHolder(private val binding: ItemChatBinding) :
+class ChatJumiViewHolder(private val binding: ItemChatJumiBinding) :
     RecyclerView.ViewHolder(binding.root) {
     fun bind(data: Chat) {
         with(binding) {
             chat = data
 
-            if (data.type == 0) { // 본인 채팅
-                layoutItemChatBackground.setBackgroundResource(R.drawable.shape_secondary_fill_start_bottom_30_rect)
-                layoutItemChat.gravity = Gravity.END
-                tvItemChat.setTextColor(root.context.colorOf(R.color.white))
-            } else if (data.type == 1) { // 상대 채팅
-                layoutItemChatBackground.setBackgroundResource(R.drawable.shape_gray3_fill_end_bottom_30_rect)
-                layoutItemChat.gravity = Gravity.START
-                tvItemChat.setTextColor(root.context.colorOf(R.color.black))
-            }
-
             if (data.button != null) {
-                rvItemChatBtn.visibility = View.VISIBLE
+                rvItemChatJumiBtn.visibility = View.VISIBLE
 
-                val layoutParams = layoutItemChatBackground.layoutParams
+                val layoutParams = layoutItemChatJumiBackground.layoutParams
                 layoutParams.width = ViewGroup.LayoutParams.MATCH_PARENT
-                layoutItemChatBackground.layoutParams = layoutParams
+                layoutItemChatJumiBackground.layoutParams = layoutParams
 
-                rvItemChatBtn.apply {
+                rvItemChatJumiBtn.apply {
                     adapter = MenuBtnAdapter().apply { submitList(data.button) }
                     layoutManager = LinearLayoutManager(
-                        binding.rvItemChatBtn.context,
+                        binding.rvItemChatJumiBtn.context,
                         LinearLayoutManager.HORIZONTAL,
                         false
                     )
                 }
             } else {
-                rvItemChatBtn.visibility = View.GONE
+                rvItemChatJumiBtn.visibility = View.GONE
             }
         }
+    }
+}
+
+class ChatUserViewHolder(private val binding: ItemChatUserBinding) :
+    RecyclerView.ViewHolder(binding.root) {
+    fun bind(data: Chat) {
+        binding.chat = data
     }
 }
