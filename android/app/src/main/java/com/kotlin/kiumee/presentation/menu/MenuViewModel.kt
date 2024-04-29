c2c4828a12b7422cd0db04669170d1110f4c574d
Eonji-sw <ejkim0625@gmail.com>
2024-04-29 18:21:38 +0900

    [FEAT] : putBilling viewModel 구현

--- android/app/src/main/java/com/kotlin/kiumee/presentation/menu/MenuViewModel.kt
+++ android/app/src/main/java/com/kotlin/kiumee/presentation/menu/MenuViewModel.kt
@@ -5,6 +5,9 @@ import androidx.lifecycle.viewModelScope
 import com.kotlin.kiumee.MainApplication
 import com.kotlin.kiumee.core.view.UiState
 import com.kotlin.kiumee.data.dto.ServicePool
+import com.kotlin.kiumee.data.dto.request.RequestBillingDto
+import com.kotlin.kiumee.data.dto.request.RequestBillingItemsDto
+import com.kotlin.kiumee.data.dto.response.ResponseBillingDto
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
@@ -13,16 +16,32 @@ class MenuViewModel : ViewModel() {
     private val _getMenu = MutableStateFlow<UiState<List<CategoryEntity>>>(UiState.Loading)
     val getMenu: StateFlow<UiState<List<CategoryEntity>>> = _getMenu
 
+    private val _putBilling = MutableStateFlow<UiState<ResponseBillingDto>>(UiState.Empty)
+    val putBilling: StateFlow<UiState<ResponseBillingDto>> = _putBilling
+
     init {
         getCategory()
     }
 
     private fun getCategory() = viewModelScope.launch {
         runCatching {
-            ServicePool.itemsApiService.getItems(MainApplication.prefs.getBusinessId()).data.map { it.toCategoryEntity() }
+            ServicePool.menuApiService.getItems(MainApplication.prefs.getBusinessId()).data.map { it.toCategoryEntity() }
         }.fold(
             { _getMenu.value = UiState.Success(it) },
             { _getMenu.value = UiState.Failure(it.message.toString()) }
+        )
+    }
+
+    fun putBilling(billingData: List<RequestBillingItemsDto>) = viewModelScope.launch {
+        kotlin.runCatching {
+            ServicePool.menuApiService.putBilling(
+                MainApplication.prefs.getBusinessId(),
+                MainApplication.prefs.getSessionId(),
+                RequestBillingDto(billingData)
+            )
+        }.fold(
+            { _putBilling.value = UiState.Success(it) },
+            { _putBilling.value = UiState.Failure(it.message.toString()) }
         )
     }
 }
