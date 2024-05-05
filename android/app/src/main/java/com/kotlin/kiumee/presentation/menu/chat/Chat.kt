3a4aac1db844a7900995438766f79eeac83e668f
Eonji-sw <ejkim0625@gmail.com>
2024-05-05 20:28:13 +0900

    [MOD] : chat data class 수정(주석)

--- android/app/src/main/java/com/kotlin/kiumee/presentation/menu/chat/Chat.kt
+++ android/app/src/main/java/com/kotlin/kiumee/presentation/menu/chat/Chat.kt
@@ -10,3 +10,31 @@ data class Chat(
         const val VIEW_TYPE_USER = 1
     }
 }
+
+// data class Chat(
+//    val viewType: Int,
+//    val result: String,
+//    val suggestItems: List<String>? = null,
+//    val orderInfo: ChatOrderInfo,
+//    val pointerId: Int? = 0,
+//    val doBilling: Boolean? = false,
+//    val showOrderList: Boolean? = false,
+//    val totalPrice: Int? = 0
+// ) {
+//    companion object {
+//        const val VIEW_TYPE_JUMI = 0
+//        const val VIEW_TYPE_USER = 1
+//    }
+// }
+//
+// data class ChatOrderInfo(
+//    val items: List<ChatItems>
+// )
+//
+// data class ChatItems(
+//    val id: Int,
+//    val category: String,
+//    val name: String,
+//    val price: Int,
+//    val quantity: Int
+// )
