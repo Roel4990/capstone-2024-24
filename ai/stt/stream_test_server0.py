959a7bf559a65a5a3975277a9be3aba31ca5b0a5
boobu <65993897+kwonbooyeon@users.noreply.github.com>
2024-05-13 07:32:58 +0900

    [update_ai/stt] stream_test_server0.py 불필요한 code 정리

--- ai/stt/stream_test_server0.py
+++ ai/stt/stream_test_server0.py
@@ -15,22 +15,11 @@ stt = au.VADAudio(input_rate=16000)
 
 
 
-'''
-def queueing_process(audio, sample_rate, frame_duration):
-    """프레임을 큐에 저장하는 함수"""
-    global frame_queue
-    frame_size = int(sample_rate * frame_duration / 1000)  # 프레임 사이즈 계산
-    for i in range(0, len(audio), frame_size):
-        frame = audio[i : i + frame_size]
-        frame_queue.append(frame.tobytes())
-'''
-
 @socketio_server.on("audio")
 def handle_audio(data):
     audio = data["audio"]
-    # audio = np.frombuffer(audio, dtype=np.int16)
-    # queueing_process(audio, sample_rate, frame_duration)
     text = stt.vad_collector(audio)
+
     socketio_server.emit("response", {"text": text})
 
 
