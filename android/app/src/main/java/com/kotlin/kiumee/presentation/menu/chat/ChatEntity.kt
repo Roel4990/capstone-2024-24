b63ee023c2077c844cc88c708c122acc2beedee3
Eonji-sw <ejkim0625@gmail.com>
2024-05-16 21:23:33 +0900

    [MOD] : chat -> chatEntity 파일명 변경 및 내부 변수 변경

---
+++ android/app/src/main/java/com/kotlin/kiumee/presentation/menu/chat/ChatEntity.kt
@@ -0,0 +1,35 @@
+package com.kotlin.kiumee.presentation.menu.chat
+
+data class ChatEntity(
+    val viewType: Int,
+    val content: String,
+    val suggestItems: List<ChatSuggestItemsEntity>? = null,
+    val orderInfo: ChatOrderInfoEntity? = null,
+    val pointerId: Int? = null,
+    val doBilling: Boolean? = false,
+    val totalPrice: Int? = 0
+) {
+    companion object {
+        const val VIEW_TYPE_JUMI = 0
+        const val VIEW_TYPE_USER = 1
+    }
+}
+
+data class ChatSuggestItemsEntity(
+    val id: Int,
+    val name: String,
+    val price: Int,
+    val imageUrl: String
+)
+
+data class ChatOrderInfoEntity(
+    val items: List<ChatOrderInfoItemsEntity>
+)
+
+data class ChatOrderInfoItemsEntity(
+    val id: Int,
+    val category: String,
+    val name: String,
+    val price: Int,
+    val quantity: Int
+)
