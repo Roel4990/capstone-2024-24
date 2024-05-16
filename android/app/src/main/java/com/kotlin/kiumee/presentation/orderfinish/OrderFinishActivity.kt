5cef275847d31889795e6622c9bb5e4e34aa4701
Eonji-sw <ejkim0625@gmail.com>
2024-05-16 21:35:12 +0900

    [MOD] : intent activity 변경

--- android/app/src/main/java/com/kotlin/kiumee/presentation/orderfinish/OrderFinishActivity.kt
+++ android/app/src/main/java/com/kotlin/kiumee/presentation/orderfinish/OrderFinishActivity.kt
@@ -5,7 +5,7 @@ import androidx.lifecycle.lifecycleScope
 import com.kotlin.kiumee.R
 import com.kotlin.kiumee.core.base.BindingActivity
 import com.kotlin.kiumee.databinding.ActivityOrderFinishBinding
-import com.kotlin.kiumee.presentation.menu.MenuActivity
+import com.kotlin.kiumee.presentation.home.HomeActivity
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
@@ -18,7 +18,7 @@ class OrderFinishActivity :
     private fun initTimer() {
         lifecycleScope.launch {
             delay(5000) // 5초
-            startActivity(Intent(this@OrderFinishActivity, MenuActivity::class.java))
+            startActivity(Intent(this@OrderFinishActivity, HomeActivity::class.java))
             // 현재 액티비티 닫기
             finish()
         }
