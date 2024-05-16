23e90ca5f50f423fbfd1cdc2b919745122661982
Eonji-sw <ejkim0625@gmail.com>
2024-05-16 21:36:32 +0900

    [MOD] : servicePool 위치 변경

--- android/app/src/main/java/com/kotlin/kiumee/presentation/home/SessionViewModel.kt
+++ android/app/src/main/java/com/kotlin/kiumee/presentation/home/SessionViewModel.kt
@@ -4,7 +4,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.kotlin.kiumee.MainApplication
 import com.kotlin.kiumee.core.view.UiState
-import com.kotlin.kiumee.data.dto.ServicePool
+import com.kotlin.kiumee.data.ServicePool
 import com.kotlin.kiumee.data.dto.response.ResponseSessionDto
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
