27299947a5ab8cc24f56aa983ccb6815cfa743d0
kwonbooyeon <qndusrnjs@gmail.com>
2024-05-19 06:39:07 +0900

    [update_ai/llm] output형태 변경, input 추가. prompt 수정전

--- ai/llm/Jumi.py
+++ ai/llm/Jumi.py
@@ -1,169 +1,26 @@
 import json
 import anthropic
+import Prompt
 
-basePrompt = '''
-    메뉴는 아래 형식을 따라 아래는 예시야.
-{
-"data": \[ { "category": "Coffee", "items": \[ { "id": 1, "name": "아메리카노", "description": "기본적인 커피입니다.", "prompt": "콜림비아 원두 커피, 잘 어울리는 메뉴는 케이크 종류임. 딸기 케이크를 추천한다.", "price": 5000, }, ] }
+from pydantic import BaseModel
+import resultData_by_pydantic as dbt
 
-위의 예시에 따르면 식당의 메뉴는 Coffee카테고리의 메뉴 id 1인 아메리카노 뿐이야. 즉.
 
-"category"아래 "items"에 있는 메뉴들이 속하는 카테고리야.
-
-"items"에서 "id”는 메뉴의 번호 "name"은 메뉴의 이름
-
-"description"는 손님에게 알려주기위한 메뉴에대한 설명이야.
-
-"prompt"는 손님을 응대하는 점원인 주미 너에게 메뉴에 대해 설명해 주는 부분이야.
-
-"price"는 메뉴의 가격 정보야.
-
-메뉴에 대한 설명은 주어진 "description"과 "prompt"내용 외에는 절대 지어내지마.
-
-"description"과 "prompt"내용이 없거나 설명이 부족하면 굳이 언급하지말고, 간단하고 중립적으로 설명해줘. 이름으로 유추하지마
-
-
-아래는 현재 장바구니 정보야. 장바구니는 손님이 결제하기전 사기위해 담아둔 메뉴 목록 데이터야.
-
-{
-
-"query": "아메리카노 하나 더 줘", "orderInfo": { "items": \[ { "id": 1, "quantity": 1 }
-]
-
-
-}
-
-}
-
-"query"는 손님이 입력한 대화 내용이야
-
-“orderinfo”에서 장바구니 정보가 담겨있어
-
-“item”에서 “id”는 손님이 주문을 위해 선택한 메뉴의 id 번호이고 이 예시에서는 id 1인 아메리카노를 담고있다는 뜻이야.
-
-"quantity”는 해당하는 메뉴를 몇개 담았는지 알려줘,
-
-대화가 끝나면 아래 형태로 답변을 생성해줘.
-
-{
-
-"response": "아메리카노 하나 더 드릴게요", "orderInfo": { "items": [ { "id": 1, "quantity": 2 }, ] }, "pointerId": 1
-
-}
-
-"response"는 너가 생성한 답변 내용을 넣으면돼
-
-"orderInfo” 이하의 내용은 장바구니 정보야. 손님의 요청으로 장바구니에 메뉴가 추가된다면
-
-이미 장바구니에 들어있는 id 1의 갯수를 늘렸다면 형식을 유지하며 따로 갯수 언급이 없다면 1개로, 손님이 요청한 갯수가 있다면 그에 맞는 갯수를 "quantity"값에 더하여 주면돼.
-
-그게 아니라 장바구니에 없는 메뉴를 추가한다면 형식을 유지하며 따로 갯수 언급이 없다면 1개로, 손님이 요청한 갯수가 있다면 그에 맞는 갯수를 "quantity"값으로 설정해서 메뉴를 “item”에 추가해주면돼
-
-메뉴 id 1를 하나 더 담고, 메뉴 id 4를 3개 추가해야한다면
-
-{
-
-"response": "아메리카노 하나 더 드릴게요", "orderInfo": { "items": [ { "id": 1, "quantity": 2 },
-
-{ "id": 4, "quantity": 3 }, ] }, "pointerId": 1
-
-}
-
-이 돼.
-
-"pointerId"는 너가 강조하고자 하는 메뉴 id를 넣어주면돼.
-
-따로 강조하고자라는 메뉴가 없다면 null을 넣어주면돼.
-
-혹은 손님에게 아메리카노를 추천해주는 상황에서는 아메리카노의 메뉴 id인 1을 넣어주면돼.
-
-예시데이터에 따른 손님에게 보이게 될 query에 따른 response예시.
-
-손님은 query를 너(주미)에게 주고 너(주미)는 손님에게 response로 대답해
-
-손님 : 여기 뭐파나요?
-
-주미 : 저희 식당의 메뉴 종류로는 Coffee가 있습니다. Coffee 메뉴를 설명해 드릴까요?
-
-손님 : 다른 음료는 없어?
-
-주미 : 네, 저희 식당에서는 Coffee만을 다루고 있습니다.
-
-손님 : 라떼 줘
-
-주미 : 저희 식당에서는 라떼는 판매하고 있지 않습니다. 대신 판매중인 메뉴인 아메리카노는 어떠신가요?
-
-손님 : 얼마야?
-
-주미 : 아메리카노는 5000원에 판매되고 있습니다. 장바구니에 담아 드릴까요?
-
-손님 : 화장실은 어디야
-
-주미 : 카운터에 문의 부탁드려요.
-
-만약 메뉴에 설명이 부족하거나 없는 메뉴에 대해 설명을 요구하면
-
-손님 : {설명이 부족한 메뉴}는 뭐야?
-
-주미 : {메뉴명}에 대해 더 이상 제공된 정보가 부족하여 자세한 내용은 카운터에 문의해주세요
-
-장바구니에 메뉴가 하나이상있다면,
-손님이 결제를 원할때
-
-주미 : {현재 장바구니 내역과 총액 설명} 결제는 결제하기 버튼을 눌러주시면 진행이 됩니다. 결제하기 버튼을 눌러주세요.
-
-형식으로 설명해줘
-
-아래의 예시는 실제 입력 데이터와 너가 생성할 대답데이터 예시야.
-
-손님 :
-
-{
-
-"query": "응 담아줘", "orderInfo": { "items": [
-
-
-]
-
-
-}
-
-}
-
-주미 :
-
-{
-
-"response": "알겠습니다. 아메리카노 1잔을 장바구니에 담을게요", "orderInfo": { "items": [ { "id": 1, "quantity": 1 }, ] }, "pointerId": null
-
-}
-
-위의 내용은 예시이며 현재 매장과는 관계가 없어.
-
-response에 입력할 대답은 최대한 간결하고 친절하게 해.
-
-무조건 위에서 알려준 형식에 맞춘 대답만을 출력해.
-
-
-
-
-아래는 실제 메뉴 데이터야.
-'''
 
 # ======
 # main
 # ======
 class Jumi():
 
-    shopPrompt = ''
-    menuPrompt = ''
-
     # init
     # input : str api(claude 3 sonnet api key), str shopInfo (형식을 맞춘 매장정보), str menuInfo (형식을 맞춘 메뉴정보)
-    def __init__(self, api, shopInfo, menuInfo):
+    def __init__(self, api, shopInfo, menuInfo, additionalInfo):
         # api call
         self.client = anthropic.Anthropic(
                 api_key = api)
+        
+        #basePrompt
+        self.basePrompt = Prompt.basePrompt
         
         # 매장정보 프롬프트
         shop = json.loads(shopInfo)
@@ -173,102 +30,226 @@ class Jumi():
 
                 ‘{shop['name']}’에 대한 설명은 ‘{shop['description']}’이야.
             '''
+
         # 메뉴정보 프롬프트
         self.menuPrompt = menuInfo
+
+        # additionalInfo
+        self.addedInfo = additionalInfo
 
 
     # jumiChat : 주미에게 질문을 하고 답변을 받는 함수
     # input : str query (질문내용과 장바구니정보가 담긴 데이터) 
     # output : str (주미의 형식에 맞춘 답변내용)
-    def jumiChat(self, query):
+    def jumiChat(self, query, history):
+        
+        history.append({
+                    "role": "user",
+                    "content": query
+                })
 
         # api통신
         message = self.client.messages.create(
             model="claude-3-sonnet-20240229",
             max_tokens=1024,
-            system = self.shopPrompt + basePrompt + self.menuPrompt ,
-            messages=[
-                {
-                    "role": "user",
-                    "content": query
-                }
-            ]
+            system = self.shopPrompt + self.basePrompt + self.menuPrompt ,
+            messages= history
         )
+        print(message)
 
+        return self.makeResultToPydantic(message.content[0].text) 
 
-        return message.content[0].text 
+    def makeResultToPydantic(self, resultMessage) -> BaseModel:
+        try:
+            message = json.loads(resultMessage)
+        except:
+            print('ERORR: result is not json')
+        result = None
+        try:
+            result = dbt.QueryResult(
+                result = message['response'],
+                orderInfo = dbt.OrderInfo(
+                      dbt.OrderInfoItem(
+                          id = item['id'], 
+                          quantity = item['quantity']
+                        ) 
+                      for item in message['orderInfo']['items']
+                    ),
+                suggestItems = message['suggestItem'],
+                pointerId = message['pointerId'],
+                doBilling = message['doBilling'],
+
+            )
+        except Exception as e:
+            print(f"ERROR: {e}")
+
+        return result
+        
     
 
 #======
 # test
 #======
-    
-shopinfotest = '''
-{
-    "name": "미도인",
-    "description": "홍대점"
-}
-'''
 
-menuinfotest = '''
-{
-  "data": [
+if __name__ == '__main__':
+  shopinfotest = '''
+  {
+    "name": "미도인",
+    "description": "강남점",
+    "prompt": "우리 가게는 강남에 위치하고 있어. 스테이크 전문점이야. 매장 위치는 서울시 정릉로 22길 302이야."
+  }
+  '''
+
+  menuinfotest = '''
+  [
     {
-      "category": "Coffee", 
+      "category": "직원 호출",
       "items": [
         {
-          "id": 1,
-          "name": "아메리카노",
-          "description": "기본적인 커피입니다.",
-          "prompt": "콜림비아 원두 커피, 잘 어울리는 메뉴는 케이크 종류임. 딸기 케이크를 추천한다.",
-          "price": 5000, 
+          "id": 25,
+          "name": "물",
+          "description": "",
+          "prompt": "이건 물이야. 손님이 요청하면 준다고 해.",
+          "price": 0
         },
         {
-          "id": 2,
-          "name": "라떼",
-          "description": "라떼가 최고.",
-          "prompt": "우유를 넣은 커피, 잘 어울리는 메뉴는 케이크 종류임. 딸기 케이크를 추천한다.",
-          "price": 5500,
+          "id": 26,
+          "name": "숟가락",
+          "description": "",
+          "prompt": "이건 숟가락이야. 손님이 요청하면 준다고 해.",
+          "price": 0
         }
       ]
     },
     {
-      "category": "Cake",
+      "category": "스테이크류",
       "items": [
         {
-          "id": 3,
-          "name": "딸기 케이크",
-          "prompt": "잘 어울리는 메뉴는 아메리카노.",
-          "price": 7000,
+          "id": 27,
+          "name": "트리플 스테이크",
+          "description": "부드러운 부채살 스테이크, 메쉬 포테이토와 대파, 특제 와인소스의 세상에 없는 환상적인 조화",
+          "prompt": "굽기를 물어보면, 굽기 조절 가능하다고 답해줘.",
+          "price": 21000
+        },
+        {
+          "id": 28,
+          "name": "미도인 등심 스테이크",
+          "description": "부드러운 등심 스테이크와 진한 육수로 우려낸 단호박 스프, 아지다마고의 한상차림",
+          "prompt": "굽기를 물어보면, 굽기 조절 가능하다고 답해줘.",
+          "price": 19000
+        }
+      ]
+    },
+    {
+      "category": "덮밥류",
+      "items": [
+        {
+          "id": 29,
+          "name": "대창 덮밥",
+          "description": "말이 필요 없는 맛. 잘 손질된 대창을 매콤한 특제 양념과 불맛나게 볶아낸 특별 덮밥",
+          "prompt": "",
+          "price": 12500
+        },
+        {
+          "id": 30,
+          "name": "대창 큐브 스테이크 덮밥",
+          "description": "스테이크와 대창을 함께 즐길 수 있는 미도인만의 특별한 콜라보 메뉴",
+          "prompt": "",
+          "price": 13000
+        },
+        {
+          "id": 31,
+          "name": "화산 불백 덮밥",
+          "description": "불맛 나는 돼지고기를 화산 모양으로 형상화한 비쥬얼 갑 강추 덮밥",
+          "prompt": "",
+          "price": 13000
+        }
+      ]
+    },
+    {
+      "category": "음료 메뉴",
+      "items": [
+        {
+          "id": 32,
+          "name": "청포도 에이드",
+          "description": "상큼한 청포도 에이드",
+          "prompt": "청포도가 생으로 들어가있어.",
+          "price": 7000
+        },
+        {
+          "id": 33,
+          "name": "복숭아 에이드",
+          "description": "새콤달콤한 복숭아 에이드",
+          "prompt": "복숭아가 생으로 들어가있어.",
+          "price": 7000
         }
       ]
     }
   ]
-}
-'''
+  '''
 
-querytest = '''
-{
-  "query": "아메리카노 하나 더 줘", 
-  "orderInfo": { 
-    "items": [
-      {
-        "id": 1, 
-        "quantity": 1
-      },
-      {
-        "id": 3,
-        "quantity": 1
-      }
-    ]
+  additiontest = '''
+  [
+    {
+      "question": "화장실은 어디야?",
+      "answer": "화장실은 나가서 왼쪽에 있습니다.",
+      "items": null
+    },
+    {
+      "question": "추천 메뉴는 뭐야?",
+      "answer": "미도인 스테이크와 대창 덮밥입니다.",
+      "items": [1,2]
+    }
+  ]
+  '''
+
+
+  #api키 넣어주세요
+  api = input('enter your claude 3 sonnet api key. : ')
+
+  #대화 선언
+  jumi = Jumi(api, shopinfotest, menuinfotest, additiontest)
+
+
+  historytest = [
+    {
+      "role": "user",
+      "content": "청포도 에이드 한잔 줘"
+    },
+    {
+      "role": "assistant",
+      "content": "{\"response\": \"알겠습니다. 청포도 에이드 1잔을 장바구니에 담겠습니다.\",\"orderInfo\": {\"items\": [{\"id\": 32, \"quantity\": 1}]},\"pointerId\": 32, \"doBilling\": false}"
+    },
+    {
+      "role": "user",
+      "content": "주문한 거 다 없애줘"
+    },
+    {
+      "role": "assistant",
+      "content": "{\"response\": \"알겠습니다.\",\"orderInfo\": {\"items\": []},\"pointerId\": null, \"doBilling\": false}"
+    }
+  ]
+  
+
+  querytest = '''
+  {
+    "query": "아메리카노 하나 더 줘", 
+    "orderInfo": { 
+      "items": [
+        {
+          "id": 1, 
+          "quantity": 1
+        },
+        {
+          "id": 3,
+          "quantity": 1
+        }
+      ]
+    }
   }
-}
-'''
-#api키 넣어주세요
-api = input('enter your claude 3 sonnet api key. : ')
+  '''
 
-#대화 선언
-jumi = Jumi(api, shopinfotest, menuinfotest)
 
-#message출력
-print(jumi.jumiChat(querytest))
+
+  #message출력
+  print(jumi.jumiChat(querytest,historytest))
