dbb4f87f73a4dd563a0239e39f4126dc5431831d
Eonji-sw <ejkim0625@gmail.com>
2024-05-20 22:55:21 +0900

    [MOD] : request/response 인자 변경

--- android/app/src/main/java/com/kotlin/kiumee/presentation/menu/cart/CartEntity.kt
+++ android/app/src/main/java/com/kotlin/kiumee/presentation/menu/cart/CartEntity.kt
@@ -1,21 +1,7 @@
 package com.kotlin.kiumee.presentation.menu.cart
 
-import com.kotlin.kiumee.data.dto.request.RequestBillingItemsDto
-import com.kotlin.kiumee.data.dto.request.RequestPromptOrderInfoItemsDto
-
 data class CartEntity(
     val id: Int,
     val name: String,
-    val count: Int,
     val price: Int
-) {
-    fun toRequestBillingItemsDto() = RequestBillingItemsDto(
-        id,
-        count
-    )
-
-    fun toRequestPromptOrderInfoItemsDto() = RequestPromptOrderInfoItemsDto(
-        id,
-        count
-    )
-}
+)
