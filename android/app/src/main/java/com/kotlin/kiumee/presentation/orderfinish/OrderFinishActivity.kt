1eb7e5db1d17ff8af76ceb8b48c135c810d26210
Eonji-sw <ejkim0625@gmail.com>
2024-05-22 22:12:46 +0900

    [MOD] : 뒤로가기 비활성화 구현

--- android/app/src/main/java/com/kotlin/kiumee/presentation/orderfinish/OrderFinishActivity.kt
+++ android/app/src/main/java/com/kotlin/kiumee/presentation/orderfinish/OrderFinishActivity.kt
@@ -1,6 +1,7 @@
 package com.kotlin.kiumee.presentation.orderfinish
 
 import android.content.Intent
+import androidx.activity.OnBackPressedCallback
 import androidx.lifecycle.lifecycleScope
 import com.kotlin.kiumee.R
 import com.kotlin.kiumee.core.base.BindingActivity
@@ -13,14 +14,22 @@ class OrderFinishActivity :
     BindingActivity<ActivityOrderFinishBinding>(R.layout.activity_order_finish) {
     override fun initView() {
         initTimer()
+        addOnBackPressedCallback()
+    }
+
+    private fun addOnBackPressedCallback() {
+        val callback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {}
+        }
+        this.onBackPressedDispatcher.addCallback(this, callback)
     }
 
     private fun initTimer() {
         lifecycleScope.launch {
             delay(5000) // 5초
-            startActivity(Intent(this@OrderFinishActivity, HomeActivity::class.java))
-            // 현재 액티비티 닫기
-            finish()
+            Intent(this@OrderFinishActivity, HomeActivity::class.java).apply {
+                flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            }.let { startActivity(it) }
         }
     }
 }
