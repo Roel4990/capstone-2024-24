116f55a7ae31523b13190236482b2f6ce7241092
Eonji-sw <ejkim0625@gmail.com>
2024-05-28 21:06:46 +0900

    [FEAT] : 로딩뷰 추가

--- android/app/src/main/java/com/kotlin/kiumee/presentation/store/StoreActivity.kt
+++ android/app/src/main/java/com/kotlin/kiumee/presentation/store/StoreActivity.kt
@@ -10,6 +10,7 @@ import com.kotlin.kiumee.R
 import com.kotlin.kiumee.core.base.BindingActivity
 import com.kotlin.kiumee.core.view.UiState
 import com.kotlin.kiumee.databinding.ActivityStoreBinding
+import com.kotlin.kiumee.presentation.LoadingActivity
 import com.kotlin.kiumee.presentation.home.HomeActivity
 import com.kotlin.kiumee.presentation.login.LoginActivity
 import kotlinx.coroutines.flow.launchIn
@@ -18,6 +19,7 @@ import timber.log.Timber
 
 class StoreActivity : BindingActivity<ActivityStoreBinding>(R.layout.activity_store) {
     private val storeViewModel by viewModels<StoreViewModel>()
+    private val loadingDialog by lazy { LoadingActivity(this) }
 
     override fun initView() {
         initHomeBtnClickListener()
@@ -35,10 +37,22 @@ class StoreActivity : BindingActivity<ActivityStoreBinding>(R.layout.activity_st
     private fun initObserve() {
         storeViewModel.getStore.flowWithLifecycle(lifecycle).onEach {
             when (it) {
-                is UiState.Success -> initFormAdapter(it.data)
-                is UiState.Failure -> Timber.d("실패 : $it")
-                is UiState.Loading -> Timber.d("로딩중")
-                is UiState.Empty -> Timber.d("empty")
+                is UiState.Success -> {
+                    loadingDialog.dismiss()
+                    initFormAdapter(it.data)
+                }
+                is UiState.Failure -> {
+                    loadingDialog.dismiss()
+                    Timber.d("실패 : $it")
+                }
+                is UiState.Loading -> {
+                    loadingDialog.show()
+                    Timber.d("로딩중")
+                }
+                is UiState.Empty -> {
+                    loadingDialog.dismiss()
+                    Timber.d("empty")
+                }
             }
         }.launchIn(lifecycleScope)
     }
